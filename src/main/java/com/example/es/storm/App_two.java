49618db1966730deb10ce9049fbc3f14307d478d
mazeyi <mazeyi@dongao.com>
2018-07-13 00:16:48 +0800

    storm

---
+++ src/main/java/com/example/es/storm/App_two.java
@@ -0,0 +1,8 @@
+package com.example.es.storm;
+
+public class App_two {
+
+    public static void main(String[] args){
+        System.out.println(111);
+    }
+}
---
+++ src/main/java/com/example/es/storm/ReportBolt.java
@@ -0,0 +1,63 @@
+package com.example.es.storm;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReportBolt extends BaseRichBolt {
+
+
+    private HashMap<String, Long> counts = null;//保存单词和对应的计数
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        // TODO Auto-generated method stub
+        this.counts = new HashMap<String, Long>();
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        // TODO Auto-generated method stub
+
+        String word = tuple.getStringByField("word");
+        Long count = tuple.getLongByField("count");
+        this.counts.put(word, count);
+
+        //实时输出
+        System.out.println(111);
+        System.out.println("结果:"+this.counts);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        // TODO Auto-generated method stub
+        //这里是末端bolt，不需要发射数据流，这里无需定义
+    }
+
+    /**
+     * cleanup是IBolt接口中定义
+     * Storm在终止一个bolt之前会调用这个方法
+     * 本例我们利用cleanup()方法在topology关闭时输出最终的计数结果
+     * 通常情况下，cleanup()方法用来释放bolt占用的资源，如打开的文件句柄或数据库连接
+     * 但是当Storm拓扑在一个集群上运行，IBolt.cleanup()方法不能保证执行（这里是开发模式，生产环境不要这样做）。
+     */
+    @Override
+    public void cleanup(){
+        System.out.println("---------- FINAL COUNTS -----------");
+
+        ArrayList<String> keys = new ArrayList<String>();
+        keys.addAll(this.counts.keySet());
+        Collections.sort(keys);
+        for(String key : keys){
+            System.out.println(key + " : " + this.counts.get(key));
+        }
+        System.out.println("----------------------------");
+    }
+}
