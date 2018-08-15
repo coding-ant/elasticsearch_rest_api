0b04aad2818c85267637e28ffb051bbce2b35df3
mario <tatami06@163.com>
2018-08-16 00:28:47 +0800

    111

--- src/main/java/com/example/es/util/Demo.java
+++ src/main/java/com/example/es/util/Demo.java
@@ -1,5 +1,10 @@
 package com.example.es.util;
 
+        import java.util.ArrayList;
+        import java.util.HashSet;
+        import java.util.LinkedList;
+        import java.util.List;
+
 /**
  * Created by Mario on 2018/7/26.
  */
@@ -11,15 +16,18 @@ public class Demo {
 
     public static void main(String[] args){
 
+           System.out.println(1);
+        ArrayList list = new ArrayList<>();
+        List list1 = new LinkedList();
     }
 }
 
 
- class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
-      public void test(){
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+    public void test(){
 
-      }
-  }
+    }
+}
