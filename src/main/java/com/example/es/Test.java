0a748f63313a327c01337006cac15e93ff033a66
mario <tatami06@163.com>
2018-09-10 22:59:14 +0800

    first and second and third

---
+++ src/main/java/com/example/es/Test.java
@@ -0,0 +1,16 @@
+package com.example.es;
+
+public class Test {
+
+    public void test(){
+        System.out.println("first line........");
+    }
+
+    public void test2(){
+        System.out.println("second line........");
+    }
+
+    public void test3(){
+        System.out.println("third line.........");
+    }
+}
